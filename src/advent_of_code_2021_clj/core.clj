6f99d78b9ec5de011b3d895553050e6ea3970008
Ivan Sharamet <isharamet@gmail.com>
2021-12-01 23:45:09 +0200

    Add day 1 solutions

--- src/advent_of_code_2021_clj/core.clj
+++ src/advent_of_code_2021_clj/core.clj
@@ -1,13 +1,17 @@
 (ns advent-of-code-2021-clj.core
   (:gen-class)
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [advent-of-code-2021-clj.day01 :as day01]))
 
 
 (defn read-input
-  [path]
-  (slurp (io/resource path)))
-
+  [day]
+  (slurp (io/resource (str day ".txt"))))
 
 (defn -main
-  [& args]
-  (read-input "day01.txt"))
+  [day]
+  (case day
+    "day01" (println
+             (day01/part1 (read-input "day01"))
+             (day01/part2 (read-input "day01")))))
+
--- src/advent_of_code_2021_clj/day01.clj
+++ src/advent_of_code_2021_clj/day01.clj
@@ -1,2 +1,32 @@
-(ns advent-of-code-2021-clj.day01)
+(ns advent-of-code-2021-clj.day01
+  (:require [clojure.string :as str]))
 
+(defn- parse-input
+  [input]
+  (->> input
+       (str/split-lines)
+       (map #(Integer/parseInt %))))
+
+(defn- count-increase
+  [xs]
+  (->> xs
+       (partition 2 1)
+       (map (fn [[l r]] (- r l)))
+       (filter pos-int?)
+       (count)))))
+
+(defn part1
+  [input]
+  (->> input
+       (parse-input)
+       (partition 2 1)
+       (map (fn [[l r]] (- r l)))
+       (filter pos-int?)
+       (count))))
+
+(defn part2
+  [input]
+  (->> input
+       (parse-input)
+       (partition 3 1)
+       (map #(reduce + %))))
