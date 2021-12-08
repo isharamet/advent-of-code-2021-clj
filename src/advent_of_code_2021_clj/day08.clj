18f5c1351a3e6764c714bbc9d40117fa67873a4e
Ivan Sharamet <isharamet@gmail.com>
2021-12-08 23:46:33 +0200

    Add day 8 (part 2) solution

--- src/advent_of_code_2021_clj/day08.clj
+++ src/advent_of_code_2021_clj/day08.clj
@@ -1,32 +1,61 @@
 (ns advent-of-code-2021-clj.day08
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as cset]))
 
-(def digit-segments
-  {1 2
-   2 5
-   3 5
-   4 4
-   5 5
-   6 6
-   7 3
-   8 7
-   9 6
-   0 6})
+(def digits-mapping
+  {1 (fn [x _]
+       (= 2 (count x)))
+   2 (fn [x m]
+       (and (= 5 (count x))
+            (not (cset/subset? x (m 9)))))
+   3 (fn [x m]
+       (and (= 5 (count x))
+            (cset/subset? (m 7) x)))
+   4 (fn [x _]
+       (= 4 (count x)))
+   5 (fn [x m]
+       (and (= 5 (count x))
+            (cset/subset? x (m 6))))
+   6 (fn [x m]
+       (and (= 6 (count x))
+            (not (cset/subset? (m 7) x))))
+   7 (fn [x _]
+       (= 3 (count x)))
+   8 (fn [x _]
+       (= 7 (count x)))
+   9 (fn [x m]
+       (and (= 6 (count x))
+            (cset/subset? (m 4) x)))
+   0 (fn [x m]
+       (and (= 6 (count x))
+            (and (cset/subset? (m 7) x)
+                 (not (cset/subset? (m 4) x)))))})
 
-(def unique-lengths
-  #{2, 3, 4, 7})
+(def mapping-order
+  [1 4 7 8 6 9 0 2 3 5])
+
+(defn- find-signal
+  [signals i raw-signals]
+  (let [f (digits-mapping i)
+        s (first (filter #(f % signals) raw-signals))]
+    (assoc signals i s)))
 
 (defn- parse-line
   [line]
   (->> line
        (#(str/split % #" \| "))
-       (map #(str/split % #" "))))
+       (map #(str/split % #" "))
+       (map #(map to-array %))
+       (map #(map set %))))
 
 (defn- parse-input
   [input]
   (->> input
        (str/split-lines)
        (map parse-line)))
+
+(def unique-lengths
+  #{2 3 4 7})
 
 (defn- solve1
   [signals]
@@ -37,13 +66,37 @@
        (map count)
        (reduce +)))
 
+(defn- map-output
+  [[signals output]]
+  (->> mapping-order
+       (reduce #(find-signal %1 %2 signals) {})
+       (cset/map-invert)
+       (#(map % output))))
+
+(defn- build-number
+  [coll]
+  (loop [coll coll
+         acc 0]
+    (if (empty? coll)
+      acc
+      (recur (rest coll)
+             (+ (* acc 10) (first coll))))))
+
+(defn- solve2
+  [input]
+  (->> input
+       (map map-output)
+       (map build-number)
+       (reduce +)))
+
 (defn part1
   [input]
   (->> input
        (parse-input)
        (solve1)))
 
-;; (defn part2
-;;   [input]
-;;   (->> input
-;;        (parse-input)))
+(defn part2
+  [input]
+  (->> input
+       (parse-input)
+       (solve2)))
