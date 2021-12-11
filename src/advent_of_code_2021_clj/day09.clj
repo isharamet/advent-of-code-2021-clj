46baffa3b5e77f12067410249b6fa7f300bb11ef
Ivan Sharamet <isharamet@gmail.com>
2021-12-11 15:54:26 +0200

    Minor refactoring

--- src/advent_of_code_2021_clj/day09.clj
+++ src/advent_of_code_2021_clj/day09.clj
@@ -20,47 +20,47 @@
        (into [])))
 
 (defn- is-low-point?
-  [[row col] dim coll]
-  (let [x (mx/get-val [row col] coll)
+  [[row col] dim m]
+  (let [x (mx/get-val [row col] m)
         adj (mx/gen-neighbours [row col] dim)]
     (->> adj
          (filter #(mx/valid-coord? % dim))
-         (map #(mx/get-val % coll))
+         (map #(mx/get-val % m))
          (every? #(> % x)))))
 
 (defn- find-low-points
-  [coll]
-  (let [dim (mx/dimensions coll)
+  [m]
+  (let [dim (mx/dimensions m)
         coords (mx/gen-coords dim)]
-    (filter #(is-low-point? % dim coll) coords)))
+    (filter #(is-low-point? % dim m) coords)))
 
 (defn- solve1
-  [coll]
-  (->> coll
+  [m]
+  (->> m
        (find-low-points)
-       (map #(mx/get-val % coll))
+       (map #(mx/get-val % m))
        (map inc)
        (reduce +)))
 
 (defn- basin-size
-  [[row col :as coord] dim coll prev visited]
+  [[row col :as coord] dim m prev visited]
   (if (and (mx/valid-coord? coord dim)
            (not (contains? visited coord)))
-    (let [x (mx/get-val coord coll)]
+    (let [x (mx/get-val coord m)]
       (if (or (nil? prev) (> 9 x prev))
         (let [visited (conj visited coord)
               {top :size
                visited :visited}
-              (basin-size [(inc row) col] dim coll x visited)
+              (basin-size [(inc row) col] dim m x visited)
               {bottom :size
                visited :visited}
-              (basin-size [(dec row) col] dim coll x visited)
+              (basin-size [(dec row) col] dim m x visited)
               {left :size
                visited :visited}
-              (basin-size [row (dec col)] dim coll x visited)
+              (basin-size [row (dec col)] dim m x visited)
               {right :size
                visited :visited}
-              (basin-size [row (inc col)] dim coll x visited)]
+              (basin-size [row (inc col)] dim m x visited)]
           {:size (+ 1 top bottom left right)
            :visited visited})
         {:size 0
@@ -69,10 +69,10 @@
      :visited visited}))
 
 (defn- solve2
-  [coll]
-  (->> coll
+  [m]
+  (->> m
        (find-low-points)
-       (map #(basin-size % (mx/dimensions coll) coll nil #{}))
+       (map #(basin-size % (mx/dimensions m) m nil #{}))
        (map :size)
        (sort-by #(- %))
        (take 3)
